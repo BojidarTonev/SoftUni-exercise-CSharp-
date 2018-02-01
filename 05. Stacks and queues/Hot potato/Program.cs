1ac71b2a1814d1f817585b3564e6233712fe53d9
Bojidar Tonev <32268977+BojidarTonev@users.noreply.github.com>
2018-02-01 14:42:58 +0200

    exercise with stack and queue

---
+++ 05. Stacks and queues/Hot potato/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hot_potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> queu = new Queue<string>(input);
+
+            while (queu.Count > 1)
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    queu.Enqueue(queu.Dequeue());
+                }
+                Console.WriteLine($"Removed {queu.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {queu.Dequeue()}");
+        }
+    }
+}
---
+++ 05. Stacks and queues/Hot potato/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Hot potato")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Hot potato")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d90c7a22-c673-4c8d-8e70-f8143a9b195b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
