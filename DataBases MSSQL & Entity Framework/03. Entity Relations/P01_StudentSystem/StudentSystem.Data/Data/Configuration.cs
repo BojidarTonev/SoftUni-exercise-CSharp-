93a73f9109308b3620d47dd55cb9166cc6981c0d
Bojidar Tonev <32268977+BojidarTonev@users.noreply.github.com>
2018-07-09 00:34:59 +0300

    .

--- DataBases MSSQL & Entity Framework/03. Entity Relations/P01_StudentSystem/StudentSystem.Data/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace StudentSystem.Data
-{
-    public class Configuration
-    {
-        public const string connectionString = "Server=.;Database=StudentSystem;Integrated Security=True";
-    }
-}
--- DataBases MSSQL & Entity Framework/03. Entity Relations/P03_FootballBetting/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace P03_FootballBetting.Data
-{
-    public class Configuration
-    {
-        public const string Connection = "Server=.;Database=FootballBetting;Integrated Security=True;";
-    }
-}
